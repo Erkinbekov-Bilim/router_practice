2a12764aca45b758eacf8a55371e121c27059014
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-01-05 17:43:27 +0600

    complete Biography page implementation

--- src/containers/EdwardSnowden/Biography/Biography.tsx
+++ src/containers/EdwardSnowden/Biography/Biography.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import './Biography.css';
 import type { IEdwardSnowdenInfo } from '../../../types';
+import RenderPage from '../../../components/RenderPage/RenderPage';
 
 interface IBiographyProps {
   biography: IEdwardSnowdenInfo[];
 }
 
-const Biography: React.FC<IBiographyProps> = ({biography}) => {
-  return <div></div>;
+const Biography: React.FC<IBiographyProps> = ({ biography }) => {
+  return (
+    <>
+      <section className="biography">
+        <div className="biography-content">
+          <div className="biography-image">
+            <img
+              src="/src/data/edwardSnowden/images/snowden_biography_img.png"
+              alt="snowden"
+            />
+          </div>
+          <div className="home-content-info">
+            {biography.map((info) => (
+              <RenderPage info={info} key={info.id} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Biography;
