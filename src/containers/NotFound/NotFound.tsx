63e3a9a198e96483cb84c79fc1bca50e2421312f
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-01-05 19:01:38 +0600

    complete not-found page implementation

--- src/containers/NotFound/NotFound.tsx
+++ src/containers/NotFound/NotFound.tsx
@@ -2,7 +2,20 @@ import React from 'react';
 import './NotFound.css';
 
 const NotFound = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="not-found">
+        <h1 className="not-found-title">404 - Page Not Found</h1>
+        <div className='not-found-img-block'>
+          <img
+            className="not-found-img"
+            src="/src/assets/photos/sad_or_angry_smile.png"
+            alt="404"
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
