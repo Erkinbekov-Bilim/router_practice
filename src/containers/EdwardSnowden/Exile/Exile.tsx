a88c5e397193c60f17bee0b51a28cecfd4524be8
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-01-05 18:14:11 +0600

    complete exile page implementation

--- src/containers/EdwardSnowden/Exile/Exile.tsx
+++ src/containers/EdwardSnowden/Exile/Exile.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import './Exile.css';
 import type { IEdwardSnowdenInfo } from '../../../types';
+import RenderPage from '../../../components/RenderPage/RenderPage';
 
 interface IExileProps {
   exile: IEdwardSnowdenInfo[];
 }
 
 const Exile: React.FC<IExileProps> = ({ exile }) => {
-  return <div>Exile</div>;
+  return (
+    <>
+      <section className="exile">
+        <div className="exile-content">
+          <div className="exile-image">
+            <img
+              src="/src/data/edwardSnowden/images/snowden_exile_img.png"
+              alt="snowden"
+            />
+          </div>
+          <div className="exile-content-info">
+            {exile.map((info) => (
+              <RenderPage info={info} key={info.id} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Exile;
